fd335485b0ab1e1568f823a970bbe2c1e61dd4d2
EdwardShanahan9 <193254185+EdwardShanahan9@users.noreply.github.com>
2025-01-18 12:32:38 +0000

    Add isEmailValid function
    - isEmailValid add error classes if validateEmail function return false, If validateEmail returns true popup element will be displayed and signup form will disappear

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,33 @@
+const formElement = document.querySelector("#form");
+
 const validateEmail = (email) => {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailRegex.test(email);
 };
 
-console.log(validateEmail("email!email.com"));
+const isEmailValid = () => {
+  const emailValue = document.querySelector("#email").value;
+  const isValid = validateEmail(emailValue);
+  const errorMessageElement = document.querySelector(".signup__error");
+  const emailInputElement = document.querySelector("#email");
+  const popupElement = document.querySelector(".popup");
+  const signupElement = document.querySelector(".signup");
 
-console.log("Hello");
+  if (isValid) {
+    popupElement.style.display = "block";
+    signupElement.style.display = "none";
+  } else {
+    errorMessageElement.classList.add("error");
+    emailInputElement.classList.add("error");
+  }
+};
+
+const validateForm = (event) => {
+  event.preventDefault();
+
+  isEmailValid();
+};
+
+formElement.addEventListener("submit", (event) => {
+  validateForm(event);
+});
